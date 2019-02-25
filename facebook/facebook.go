2653a769d34610f68f31c4f2029fb42e491b987c
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2019-02-25 15:33:38 +0800

    Add way to add client id and secret

--- facebook/facebook.go
+++ facebook/facebook.go
@@ -6,10 +6,20 @@ type FacebookProvider struct {
 	cfg oauth2.Config
 }
 
+var Provider FacebookProvider
+
 func (f FacebookProvider) Login(redirectURL, state string) error {
 	return nil
 }
 
 func (f FacebookProvider) Config() oauth2.Config {
 	return f.cfg
+}
+
+func (f FacebookProvider) SetClientID(client_id string) {
+	f.cfg.ClientID = client_id
+}
+
+func (f FacebookProvider) SetClientSecret(client_secret string) {
+	f.cfg.ClientSecret = client_secret
 }
